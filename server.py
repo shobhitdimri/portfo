1e6a335c939d60009a6dffa7439a86c49847e87a
shobhitdimri <68077020+shobhitdimri@users.noreply.github.com>
2020-07-27 14:01:25 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,57 +1,40 @@
-from flask import Flask, render_template,request,redirect
+from flask import Flask, render_template, url_for, request, redirect
 import csv
-app = Flask(__name__)
-print(__name__)
 
-@app.route('/index.html')
-def home():
+app = Flask(__name__)
+
+@app.route('/')
+def my_home():
     return render_template('index.html')
 
-
-@app.route('/about.html')
-def about():
-    return render_template('about.html')
-
-@app.route('/works.html')
-def works():
-    return render_template('works.html')
-
-@app.route('/contact.html')
-def contact():
-    return render_template('contact.html')
-
-@app.route('/work.html')
-def work():
-    return render_template('work.html')
-
-@app.route('/thank you.html')
-def thank():
-    return render_template('thank you.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
 def write_to_file(data):
-    with open('database.txt', mode='a') as database:
-       email =data["email"]
-       subject =data["subject"]
-       file=database.write('\n{email},{subject},{message}')
+  with open('database.txt', mode='a') as database:
+    email = data["email"]
+    subject = data["subject"]
+    message = data["message"]
+    file = database.write(f'\n{email},{subject},{message}')
 
 def write_to_csv(data):
-    with open('database.csv',mode='a',newline='') as database2:
-       email=data["email"]
-       subject=data["subject"]
-       message=data["message"]
-       csv_writer=csv.writer(database2,  quotechar='"', quoting=csv.QUOTE_MINIMAL)
-       csv_writer.writerow([email,subject,message])
+  with open('database.csv', newline='', mode='a') as database2:
+    email = data["email"]
+    subject = data["subject"]
+    message = data["message"]
+    csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    csv_writer.writerow([email,subject,message])
 
 
-
-@app.route('/Submit_form', methods=['POST', 'GET'])
-def Submit_form():
-    if request.method=='POST':
-        try:
-         data=request.form.to_dict()
-         write_to_csv(data)
-         return redirect('/thank you.html')
-        except:
-            return'not saved on database'
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+      try:
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return redirect('/thank you.html')
+      except:
+        return 'did not save to database'
     else:
-        return  'something went wrong!'
+      return 'something went wrong. Try again!'
